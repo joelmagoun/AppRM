97f2c89d521af4c9b903c45259bc29a8a62b14a5
Joel <joel.magoun@gmail.com>
2025-06-01 23:26:10 +0000

    changing anonkey and lowering to version 1.0.2.0

--- apps/apprm/lib/configs/supabase.dart
+++ apps/apprm/lib/configs/supabase.dart
@@ -1,7 +1,7 @@
 class SupabaseConfig {
   static const String url = 'https://wzrqeslqeysdbspfqbff.supabase.co';
   static const String anonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind6cnFlc2xxZXlzZGJzcGZxYmZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI3MTEyNjAsImV4cCI6MjAzODI4NzI2MH0.UuAB9S9ANEfAf8UX8TMF1b_5NjxxIDYuqfaUlSfuhHE';
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind6cnFlc2xxZXlzZGJzcGZxYmZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA1MTcxMjksImV4cCI6MjA1NjA5MzEyOX0.rGzlK4-sAHgGA4jsXkEh1m7mEF9hOCZSOx4isMZ4G4U';
 
   static const String storageBucket = '';
 }
