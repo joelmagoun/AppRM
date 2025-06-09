1b0d37fe2f2b286b7c9049e5fa415bbde2d64e06
Joel <joel.magoun@gmail.com>
2025-06-09 17:46:34 +0000

    v.1.1.1.17 - trying to change completed to text field

--- apps/apprm/lib/router.g.dart
+++ apps/apprm/lib/router.g.dart
@@ -77,7 +77,7 @@ extension $SupabaseSignUpRouteExtension on SupabaseSignUpRoute {
 extension $SupabaseLoginRouteExtension on SupabaseLoginRoute {
   static SupabaseLoginRoute _fromState(GoRouterState state) =>
       SupabaseLoginRoute(
-        email: state.uri.queryParameters['email'],
+        email: state.queryParameters['email'],
       );
 
   String get location => GoRouteData.$location(
@@ -100,7 +100,7 @@ extension $SupabaseLoginRouteExtension on SupabaseLoginRoute {
 extension $AzureB2CLoginRouteExtension on AzureB2CLoginRoute {
   static AzureB2CLoginRoute _fromState(GoRouterState state) =>
       AzureB2CLoginRoute(
-        email: state.uri.queryParameters['email'],
+        email: state.queryParameters['email'],
       );
 
   String get location => GoRouteData.$location(
@@ -122,7 +122,7 @@ extension $AzureB2CLoginRouteExtension on AzureB2CLoginRoute {
 
 extension $Auth0LoginRouteExtension on Auth0LoginRoute {
   static Auth0LoginRoute _fromState(GoRouterState state) => Auth0LoginRoute(
-        email: state.uri.queryParameters['email'],
+        email: state.queryParameters['email'],
       );
 
   String get location => GoRouteData.$location(
@@ -331,8 +331,8 @@ extension $ExternalObjectListingRouteExtension on ExternalObjectListingRoute {
   static ExternalObjectListingRoute _fromState(GoRouterState state) =>
       ExternalObjectListingRoute(
         externalObjectType: state.pathParameters['externalObjectType']!,
-        objectType: state.uri.queryParameters['object-type'],
-        objectId: state.uri.queryParameters['object-id'],
+        objectType: state.queryParameters['object-type'],
+        objectId: state.queryParameters['object-id'],
       );
 
   String get location => GoRouteData.$location(
@@ -358,8 +358,8 @@ extension $ExternalObjectDetailRouteExtension on ExternalObjectDetailRoute {
       ExternalObjectDetailRoute(
         externalObjectType: state.pathParameters['externalObjectType']!,
         externalObjectId: state.pathParameters['externalObjectId']!,
-        objectType: state.uri.queryParameters['object-type'],
-        objectId: state.uri.queryParameters['object-id'],
+        objectType: state.queryParameters['object-type'],
+        objectId: state.queryParameters['object-id'],
       );
 
   String get location => GoRouteData.$location(
