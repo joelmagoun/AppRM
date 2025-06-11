b515542c35793cb996031e374fb4759f53c33966
Joel Magoun <31316715+joelmagoun@users.noreply.github.com>
2025-06-11 15:08:38 +0000

    feat: allow selecting existing screen elements

---
+++ apps/apprm/lib/features/screens/widgets/element_selection.dart
@@ -0,0 +1,49 @@
+import 'package:cached_query_flutter/cached_query_flutter.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../common_object/foundation/object_repository.dart';
+import '../../common_object/foundation/use_cases/get_screen_elements_usecase.dart';
+
+class ElementSelection extends ConsumerWidget {
+  const ElementSelection({super.key, required this.screenId});
+
+  final String screenId;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return SafeArea(
+      child: SizedBox(
+        height: 400,
+        child: QueryBuilder<List<Map<String, dynamic>>>(
+          query: Query(
+            key: ['screen_elements', 'select', screenId],
+            queryFn: () async {
+              return await GetScreenElementsUseCase(
+                objectRepository: ObjectRepository(),
+              ).execute(
+                GetScreenElementsUseCaseParams(screenId: screenId),
+              );
+            },
+          ),
+          builder: (context, state) {
+            final list = state.data ?? [];
+            return ListView.separated(
+              itemCount: list.length,
+              separatorBuilder: (_, __) => const Divider(height: 1),
+              itemBuilder: (_, idx) {
+                final item = list[idx];
+                return ListTile(
+                  title: Text(item['name'] ?? '--'),
+                  onTap: () {
+                    Navigator.of(context).pop(item);
+                  },
+                );
+              },
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
